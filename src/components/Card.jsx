97db96d14e6c55e0210daa96e9ee72035f4fc836
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2025-01-14 20:20:26 -0500

    continue to work through api call logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+
 function Card({ id }) {
-    // is there where i need use effect because i'll be calling api?
-    // https://pokeapi.co/api/v2/pokemon/{id or name}/
-    // need to pull name and image from api call
+    const [loading, setLoading] = useState(false);
+    const [pokedex, setPokedex] = useState([]);
+
+    useEffect(() => {
+        try {
+            const loadPokedex = async () => {
+                setLoading(true);
+
+                const response = await fetch(
+                    `https://pokeapi.co/api/v2/pokemon-species/${id}/`, //maybe change this back to overall dataset and remove species, then can pull name and image
+                    { mode: "cors" }
+                );
+
+                const pokedexData = await response.json();
+
+                setPokedex(pokedexData);
+                setLoading(false);
+            };
+
+            loadPokedex();
+        } catch (error) {
+            console.log(`error in api call: ${error}`);
+            setLoading(false);
+        }
+    }, [id]);
+
+    // delete this effect when done debugging
+    useEffect(() => {
+        console.log("Updated pokedex:", pokedex); // Logs updated state
+    }, [pokedex]);
 
     return (
         <div className="card">
